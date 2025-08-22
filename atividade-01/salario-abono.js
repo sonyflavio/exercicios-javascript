d0309a1e689977d47154b058b06974defee8aaeb
Flavio Serra <sonyflavio@gmail.com>
2025-08-22 14:02:12 -0300

    envio ativide 01

--- atividade-01/salario-abono.js
+++ atividade-01/salario-abono.js
@@ -1,1 +1,9 @@
-cons ler
+const leia = require('readline-sync')
+
+let salario = leia.questionFloat("Digite o salario: ")
+let abono = leia.questionFloat("Digite o abono: ")
+
+let novoSalario = salario + abono 
+
+
+console.log("Novo salário : %f" , novoSalario )
